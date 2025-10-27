3a48000798565a5d309367547dbd562d5231feec
DanielG <danielgebeyehu45@gmail.com>
2025-10-26 19:12:52 -0700

    frontend-cleanup

--- src/axiosconfig.js
+++ src/axiosconfig.js
@@ -8,7 +8,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://forum-backend-production.up.railway.app/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://intuitive-courage-production.up.railway.app/api",
   withCredentials: true,
 });
 
